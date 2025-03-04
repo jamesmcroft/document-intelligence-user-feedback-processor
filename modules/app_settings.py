69b2680920a57b294f9d6e762db74e44a6ab84c2
James Croft <jamz_c@hotmail.co.uk>
2025-03-04 11:40:12 +0000

    Archive previous content

--- modules/app_settings.py
+++
@@ -1,8 +0,0 @@
-class AppSettings:
-    def __init__(self, config: dict):
-        self.managed_identity_client_id = config['AZURE_MANAGED_IDENTITY_CLIENT_ID']
-        self.document_intelligence_endpoint = config['AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT']
-        self.document_intelligence_key = config['AZURE_DOCUMENT_INTELLIGENCE_KEY']
-        self.storage_account_name = config['AZURE_STORAGE_ACCOUNT_NAME']
-        self.storage_account_connection_string = config['AZURE_STORAGE_ACCOUNT_CONNECTION_STRING']
-        self.training_data_container_name = config['AZURE_DOCUMENT_INTELLIGENCE_TRAINING_DATA_CONTAINER_NAME']
