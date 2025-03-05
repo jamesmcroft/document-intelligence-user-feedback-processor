9101cfe92441923ef251b1d3a006786e054919d6
James Croft <jamz_c@hotmail.co.uk>
2025-03-05 20:08:14 +0000

    Update devcontainer configuration: replace .NET with Python and install dependencies

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -12,3 +12,5 @@ err() {
 # Ensure apt is in non-interactive to avoid prompts
 export DEBIAN_FRONTEND=noninteractive
 
+# Install Python dependencies
+pip --disable-pip-version-check --no-cache-dir install --user -r src/Feedback.Service/requirements.txt
